062af3b3347e0072314ea72447dab112ab5931bd
Jacob Williams <ponyboy47@gmail.com>
2017-11-14 10:05:09 -0700

    Updated to swift tools version 4.0

--- Package.swift
+++ Package.swift
@@ -1,7 +1,22 @@
-// swift-tools-version:3.1
+// swift-tools-version:4.0
 
 import PackageDescription
 
 let package = Package(
-    name: "Strings"
+    name: "Strings",
+    products: [
+        .library(
+            name: "Strings",
+            targets: ["Strings"]),
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .target(
+            name: "Strings",
+            dependencies: []),
+        .testTarget(
+            name: "StringsTests",
+            dependencies: ["Strings"]),
+    ]
 )
